305db74c09ffa05373d0f4e639c9dd4b1fbc79ee
ArchieKast <kastyushkin95@mail.ru>
2017-11-07 00:46:08 +0300

    Sign in and registering.
    Changed interaction with forms.

--- FRONTEND/jquery/forms.js
+++ FRONTEND/jquery/forms.js
@@ -4,19 +4,38 @@ $(document).ready(function(){
     
     $('.header-menu__block:last').click(function(){
         $('.form-reg').hide();
-        $('.form-enter').toggle();
+        $('.form-enter').show();
+    });
+    
+    $('.form-enter__close').mousemove(function(){
+        $('.form-enter__close').css('border-style','inset');
+    });
+    $('.form-enter__close').mouseleave(function(){
+        $('.form-enter__close').css('border-style','inherit');
+    });
+    $('.form-enter__close').click(function(){
+        $('.form-enter').hide();
     });
     
     $('.form-enter__regTitle').mousemove(function(){
         $('.form-enter__regTitle').css('color','red');
     });
-    
     $('.form-enter__regTitle').mouseleave(function(){
         $('.form-enter__regTitle').css('color','blue');
-    });
+    });  
     
     $('.form-enter__regTitle').click(function(){
         $('.form-enter').hide();
         $('.form-reg').show();
+    });
+    $('.form-reg__close').click(function(){
+        $('.form-reg').hide();
+    });
+    
+    $('.form-reg__close').mousemove(function(){
+        $('.form-reg__close').css('border-style','inset');
+    });
+    $('.form-reg__close').mouseleave(function(){
+        $('.form-reg__close').css('border-style','inherit');
     });
 });
