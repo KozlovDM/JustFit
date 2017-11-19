6b757dd1229251069d39a41fc33364bc1442c3ae
ArchieKast <kastyushkin95@mail.ru>
2017-11-19 19:26:31 +0300

    Process the server response. Attempt #2.
    Attempting to process server response.

--- FRONTEND/js/answerExecution.js
+++ FRONTEND/js/answerExecution.js
@@ -1,28 +1,18 @@
 $(document).ready(function(){
     $("#regForm").submit(function(event) {
-        if($(this).data('formstatus') !== 'submitting'){
-            var form = $(this),
-                formData = form.serialize(),
-                formUrl = form.attr('action'),
-                formMethod = form.attr('method'),
-                responseMsg = $('#signup-response');
+        var form = $(this),
+            formData = form.serialize(),
+            formUrl = form.attr('action'),
+            formMethod = form.attr('method')
         
-            $.ajax({
-                url: formUrl,
-                type: formMethod,
-                data: formData,
-                success:function(data){
-                    var responseData = jQuery.parseJSON(data),
-                        klass = 'message';
-                }
-                
-                responseMsg.fadeOut(200,function(){
-                    $(this).addClass(klass)
-                    .text(responseData.message)
-                    .fadeIn(200,function());
-                });
-            });
-        }
-        return false;
+        $.ajax({
+            url: formUrl,
+            type: formMethod,
+            data: formData,
+            success:function(data){
+                var responseData = jQuery.parseJSON(data),
+                    }
+        });
+        alert(responseData);
     });
 });
