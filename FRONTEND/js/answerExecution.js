bd1bd15517bbaf75cc1c4b6f3e57babec4989d13
ArchieKast <kastyushkin95@mail.ru>
2017-11-20 19:18:27 +0300

    Process the server response. Attempt #4.
    Attempting to process server response.

--- FRONTEND/js/answerExecution.js
+++ FRONTEND/js/answerExecution.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-    $('submit').click(function(){
-        window.location.href = "UserPage.html"
-    })
+    $('#regForm').submit(function(){
+        if($(this).data('formstatus') !== 'submitting'){
+            var form = $(this),
+                formData = form.serialize(),
+                formUrl = "http://192.168.56.1:3000/SignUp",
+                formMethod = form.attr('method');
+                
+            form.data('formstatus','submitting');
+            $.ajax({
+                url: formUrl,
+                type: formMethod,
+                data: formData,
+                success:function(data){
+                    var responseData = jQuery.parseJSON(data);
+                    alert(data);     
+                }
+            });
+            window.location.href = form.attr('action');
+        }
+        return false;
+    });
 });
