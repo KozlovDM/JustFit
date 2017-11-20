7962fe78864f172d6b715b2e4abc01c0a39f5fc2
ArchieKast <kastyushkin95@mail.ru>
2017-11-20 16:41:56 +0300

    Go to UserPage.
    An attempt to go to the UserPage after sending data from forms.

--- FRONTEND/js/answerExecution.js
+++ FRONTEND/js/answerExecution.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-    $("#regForm").submit(function(event) {
-        var form = $(this),
-            formUrl = form.attr('action')
-        $.post(formUrl, function(data) {
-            alert(data);
-        });
-    });
+    $('submit').click(function(){
+        window.location.href = "UserPage.html"
+    })
 });
