9af99c6e36c259b0693087765b775744829d8f69
ArchieKast <kastyushkin95@mail.ru>
2017-11-27 22:06:40 +0300

    Upload photos #2.
    Another attempt.

--- FRONTEND/loadingUserData.js
+++ FRONTEND/loadingUserData.js
@@ -32,33 +32,31 @@ $(document).ready(function(){
         }
     });
     
-    $('#uploadForm').on('submit', function(event){
-        if ($(this).data('formstatus') !== 'submitting' &&  window.FormData !== undefined) {
-            event.preventDefault();
-            var form = $(this),
-                formData = new FormData(form.get(0));
-            formData.append("phone", phone);
-            
-            $(this).data('formstatus','submitting');
-            $.ajax({
-                url: 'http://192.168.56.1:3000/download',
-                type: form.attr('method'),
-                contentType: false,
-                processData: false,
-                data: formData,
-                dataType: 'json',
-                success: function(data){
-                    var ref = '"' + data[0] + '"';
-                    var block = '<div class="publication"><a href=' + ref + ' target="_blank"><img src=' + ref + '></a></div>';
-                    $('.header-menu__link:eq(1)').click(function(){
+    $('#clickbutton').on('click', function(event){
+        event.preventDefault();
+        var file = document.getElementById('uploadFile').files;
+        if (files.length !== 0){
+            if (window.FormData !== undefined){
+                var data = new FormData();
+                data.append("file", files[0]);
+                data.append("phone", phone);
+                
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://192.168.56.1:3000/Upload',
+                    contentType: false,
+                    processData: false,
+                    data: data,
+                    success: function(data){
+                        var ref = '"' + data + '"';
+                        var block = '<div class="publication"><a href=' + ref + ' target="_blank"><img src=' + ref + '></a></div>';
                         $('.main-collage').append(block);
-                    });
-                },
-                error: function() {
-                    alert("Файл не может быть загружен!");
-                }
-            });
+                    },
+                    error: function(){
+                        alert("Файл не может быть загружен!");
+                    }
+                });
+            }
         }
-        return false;
-    });
+    });       
 });
